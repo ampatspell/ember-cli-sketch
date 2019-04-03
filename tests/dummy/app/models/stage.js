d89974f1ae7a8085cfbe4f4b38038d876a2da2e7
Arnis Vuskans <ampatspell@gmail.com>
2019-04-03 17:37:27 +0300

    don't persist stage props

--- tests/dummy/app/models/stage.js
+++ tests/dummy/app/models/stage.js
@@ -15,9 +15,9 @@ export default EmberObject.extend({
 
   node: node({ type: 'stage' }),
 
-  x:    attr('x', { type: 'number', initial: 0 }),
-  y:    attr('y', { type: 'number', initial: 0 }),
-  zoom: attr('zoom', { type: 'number', initial: 1, min: 0.1, decimals: 2 }),
+  x:    attr('_x', { type: 'number', initial: 0 }),
+  y:    attr('_y', { type: 'number', initial: 0 }),
+  zoom: attr('_zoom', { type: 'number', initial: 1, min: 0.1, decimals: 2 }),
 
   // thing: attr('thing', { type: 'number', initial: 0, min: prop('thingMin'), max: prop('thingMax') }),
   // thingMin: 0,
