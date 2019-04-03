c6cbdadcb3773af22892290ecb890ec823461729
Arnis Vuskans <ampatspell@gmail.com>
2019-04-03 17:09:21 +0300

    disable rotation for containers

--- addon/-private/node/frame/sized.js
+++ addon/-private/node/frame/sized.js
@@ -1,8 +1,16 @@
 import Frame, { model } from './-base';
+import { computed } from '@ember/object';
 import { serialized } from '../../util/computed';
 import { zoomed, rotated, absolute } from './-computed';
 
 const keys = [ 'x', 'y', 'width', 'height', 'rotation' ];
+
+const rotation = () => computed(`model.rotation`, 'parent.isContainer', function() {
+  if(this.parent.isContainer) {
+    return 0;
+  }
+  return this.model.rotation;
+}).readOnly();
 
 export default Frame.extend({
 
@@ -10,7 +18,7 @@ export default Frame.extend({
   y:        model('y'),
   width:    model('width'),
   height:   model('height'),
-  rotation: model('rotation'),
+  rotation: rotation(),
 
   properties:     serialized(keys),
   rotated:        rotated('properties'),
