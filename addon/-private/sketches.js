13ddff4a546206772fba79a69c05df3b40159b76
Arnis Vuskans <ampatspell@gmail.com>
2019-04-04 11:11:48 +0300

    cleanup

--- addon/-private/sketches.js
+++ addon/-private/sketches.js
@@ -1,10 +1,12 @@
 import EmberObject, { computed } from '@ember/object';
 import { getOwner } from '@ember/application';
 
+const factory = () => computed(function() {
+  return getOwner(this).factoryFor('sketch:factory').create({ sketches: this });
+}).readOnly();
+
 export default EmberObject.extend({
 
-  factory: computed(function() {
-    return getOwner(this).factoryFor('sketch:factory').create({ sketches: this });
-  }).readOnly()
+  factory: factory()
 
 });
