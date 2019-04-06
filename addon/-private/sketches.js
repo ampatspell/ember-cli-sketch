5038c1e9bd2148579ea025bad1c322941d951f11
Arnis Vuskans <ampatspell@gmail.com>
2019-04-06 22:53:59 +0300

    remove sketches.cursor

--- addon/-private/sketches.js
+++ addon/-private/sketches.js
@@ -2,19 +2,11 @@ import EmberObject, { computed } from '@ember/object';
 import { getOwner } from '@ember/application';
 
 const factory = () => computed(function() {
-  setGlobal({ sketches: this });
   return getOwner(this).factoryFor('sketch:sketches/factory').create({ sketches: this });
-}).readOnly();
-
-const create = name => computed(function() {
-  let factory = this.factory;
-  return factory[name].call(factory, { sketches: this });
 }).readOnly();
 
 export default EmberObject.extend({
 
-  factory: factory(),
-
-  cursor: create('cursor')
+  factory: factory()
 
 });
