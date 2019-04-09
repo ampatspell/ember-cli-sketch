ba269314f699d086b6f84d70d009184224291392
Arnis Vuskans <ampatspell@gmail.com>
2019-04-09 15:20:37 +0300

    add crop marks inset

--- addon/components/ui-block/sketch/stage/node/accessories/crop-marks/component.js
+++ addon/components/ui-block/sketch/stage/node/accessories/crop-marks/component.js
@@ -1,7 +1,26 @@
 import Component from '@ember/component';
 import layout from './template';
+import { style } from '../../../../-computed';
 
 export default Component.extend({
   layout,
-  classNameBindings: [ ':ui-block-sketch-stage-accessories-crop-marks' ]
+  classNameBindings: [ ':ui-block-sketch-stage-accessories-crop-marks' ],
+  attributeBindings: 'style',
+
+  inset: 0,
+
+  style: style('inset', function() {
+    let { inset } = this;
+    if(!inset) {
+      return;
+    }
+    inset = `${inset}px`;
+    return {
+      top:    inset,
+      left:   inset,
+      bottom: inset,
+      right:  inset
+    };
+  })
+
 });
