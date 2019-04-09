b85ffbe9923760c292af9e2c3611f0a2a323f6ec
Arnis Vuskans <ampatspell@gmail.com>
2019-04-09 13:23:56 +0300

    move node default property keys to addon

--- addon/-private/computed/node.js
+++ addon/-private/computed/node.js
@@ -1,7 +1,22 @@
 import { assert } from '@ember/debug';
+import { assign } from '@ember/polyfills';
 import { factory } from '../util/computed';
 
-export default opts => factory((factory, model) => factory.node(model, opts));
+const defaults = {
+  stage:      'stage',
+  parent:     'parent',
+  nodes:      'nodes',
+  type:       'type',
+  visible:    'visible',
+  selectable: 'selectable'
+};
+
+const normalize = (opts={}) => {
+  let properties = assign({}, defaults, opts.properties);
+  return assign({ properties }, opts);
+}
+
+export default opts => factory((factory, model) => factory.node(model, normalize(opts)));
 
 export const getNode = model => {
   let node = model.node;
--- tests/dummy/app/models/-node.js
+++ tests/dummy/app/models/-node.js
@@ -2,15 +2,7 @@ import { node as _node, attr as _attr, prop } from 'ember-cli-sketch/computed';
 import { assign } from '@ember/polyfills';
 
 export const node = opts => _node(assign({
-  identifier: 'dummy',
-  properties: {
-    stage:      'stage',
-    parent:     'parent',
-    nodes:      'nodes',
-    type:       'type',
-    visible:    'visible',
-    selectable: 'selectable'
-  }
+  identifier: 'dummy'
 }, opts));
 
 export const attr = (target, opts) => _attr(`doc.${target}`, opts);
