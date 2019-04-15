7557f4c4de01af68b5531818d9186a43f1f2b07d
Arnis Vuskans <ampatspell@gmail.com>
2019-04-15 22:21:28 +0300

    font spec

--- addon/-private/sketches/factory.js
+++ addon/-private/sketches/factory.js
@@ -145,7 +145,7 @@ export default EmberObject.extend({
         google: {
           'Ubuntu Mono': '400,400i,700,700i',
           'Pacifico': true,
-          'Montserrat': true,
+          'Montserrat': '100,100i,200,200i,300,300i,400,400i,500,500i,600,600i,700,700i,800,800i,900,900i:latin,latin-ext',
           'Bitter': true,
           'Amatic SC': true,
           'Chewy': true,
