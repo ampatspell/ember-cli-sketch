d06c04643053ee36b7a295030e33cb7bca924ba6
Arnis Vuskans <ampatspell@gmail.com>
2019-04-25 15:22:03 +0300

    try using img onload for deferred promise

--- addon/components/ui-block/sketch/stage/node/image/component.js
+++ addon/components/ui-block/sketch/stage/node/image/component.js
@@ -1,22 +1,56 @@
 import Component from '../-component';
 import layout from './template';
-import { style, imagePromise } from '../-computed';
+import { style } from '../-computed';
+import { computed } from '@ember/object';
+import { readOnly } from '@ember/object/computed';
+import { defer } from 'rsvp';
+import safe from '../../../../../../-private/util/safe';
+import { cancel, later } from '@ember/runloop';
 
 export default Component.extend({
   layout,
 
-  promise: imagePromise('model.url'),
-
-  style: style('model.{opacity,url}', function() {
-    let { model: { opacity, url } } = this;
-    let backgroundImage;
-    if(url) {
-      backgroundImage = `url(${url})`;
+  deferred: computed('model.url', function() {
+    let { model: { url } } = this;
+    if(!url) {
+      return;
     }
+    let deferred = defer();
+    deferred.url = url;
+    return deferred;
+  }).readOnly(),
+
+  promise: readOnly('deferred.promise'),
+
+  style: style('model.{opacity}', function() {
+    let { model: { opacity } } = this;
     return {
-      opacity,
-      backgroundImage
+      opacity
     };
-  })
+  }),
+
+  resolve: safe(function(url) {
+    let { deferred } = this;
+    if(deferred.url !== url) {
+      return;
+    }
+    deferred.resolve();
+  }),
+
+  scheduleResolve(url) {
+    cancel(this._scheduleResolve);
+    this._scheduleResolve = later(() => this.resolve(url), 100);
+  },
+
+  willDestroyElement() {
+    this._super(...arguments);
+    cancel(this._scheduleResolve);
+  },
+
+  actions: {
+    imageDidLoad(e) {
+      this.scheduleResolve(e.target.src);
+    }
+  }
 
 });
