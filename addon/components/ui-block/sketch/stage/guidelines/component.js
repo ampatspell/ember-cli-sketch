8b1c8098c6ba9818b8d0f5a1894d5c2a816f542e
Arnis Vuskans <ampatspell@gmail.com>
2019-05-29 22:12:28 +0300

    add noop guidelines component

---
+++ addon/components/ui-block/sketch/stage/guidelines/component.js
@@ -0,0 +1,7 @@
+import Component from '@ember/component';
+import layout from './template';
+
+export default Component.extend({
+  layout,
+  classNameBindings: [ ':ui-block-sketch-stage-guidelines' ],
+});
