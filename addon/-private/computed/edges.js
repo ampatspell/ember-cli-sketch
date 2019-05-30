401306b667a6ba6d95739041ab72438c46373aba
Arnis Vuskans <ampatspell@gmail.com>
2019-05-30 03:05:01 +0300

    move edges defaults to computed

--- addon/-private/computed/edges.js
+++ addon/-private/computed/edges.js
@@ -2,9 +2,39 @@ import { computed } from '@ember/object';
 
 const key = 'node.frame.hover';
 
+const _fn = frame => {
+  let { x, y, width, height } = frame;
+
+  let mid = {
+    x: x + (width / 2),
+    y: y + (height / 2)
+  };
+
+  let max = {
+    x: x + width,
+    y: y + height
+  };
+
+  return [
+    // top
+    { x: x,     y: y },
+    { x: mid.x, y: y },
+    { x: max.x, y: y },
+    // middle
+    { x: x,     y: mid.y },
+    { x: mid.x, y: mid.y },
+    { x: max.x, y: mid.y },
+    // bottom
+    { x: x,     y: max.y },
+    { x: mid.x, y: max.y },
+    { x: max.x, y: max.y },
+  ];
+};
+
 export default fn => computed(key, function() {
   let frame = this.get(key);
   if(frame) {
+    fn = fn || _fn;
     return fn.call(this, frame);
   }
 }).readOnly();
--- tests/dummy/app/models/node/rect.js
+++ tests/dummy/app/models/node/rect.js
@@ -17,32 +17,7 @@ export default Base.extend({
   fill:     attr('fill'),
   opacity:  attr('opacity', { type: 'number', min: 0, max: 1, decimals: 2 }),
 
-  edges: edges(frame => {
-    // TODO: have this as a default
-    let { x, y, width, height } = frame;
-    let mid = {
-      x: x + (width / 2),
-      y: y + (height / 2)
-    };
-    let max = {
-      x: x + width,
-      y: y + height
-    };
-    return [
-      // top
-      { x: x,     y: y },
-      { x: mid.x, y: y },
-      { x: max.x, y: y },
-      // middle
-      { x: x,     y: mid.y },
-      { x: mid.x, y: mid.y },
-      { x: max.x, y: mid.y },
-      // bottom
-      { x: x,     y: max.y },
-      { x: mid.x, y: max.y },
-      { x: max.x, y: max.y },
-    ];
-  }),
+  edges: edges(),
 
   hasRotation: true,
   hasFill: true,
