6434e6f57b2c070b6b1352446a4ed25fac626580
Arnis Vuskans <ampatspell@gmail.com>
2019-05-31 16:27:16 +0300

    cleanup

--- addon/-private/stage/guidelines.js
+++ addon/-private/stage/guidelines.js
@@ -8,18 +8,6 @@ export default EmberObject.extend({
   enabled: readOnly('stage.tools.selected.guidelines'),
   selection: readOnly('stage.selection.attached'),
 
-  // iterate selection nodes
-  // nodes has lines/edges v-left, v-middle, v-right, h-top, ...
-  // edges are in two groups -- vertical and horizontal. those are evaluated separately
-  // lookup related nodes based on edges (v and h)
-  // emit guidelines
-
-  // TODO: final lines has to be kept the same for rerenders. This kills performance
-
-  // list of edges of non-selected nodes
-  // list of edges of selected nodes
-  // of edge - edge passes threshold, it becomes a gudeline
-
   // TODO: temp
   edges: computed('selection.@each.edges', function() {
     return this.selection.reduce((arr, node) => {
@@ -36,23 +24,9 @@ export default EmberObject.extend({
     });
   }).readOnly(),
 
-  // lines: computed(function() {
-  //   return [
-  //     { type: 'horizontal', x: 630, y: 425, length: 500 },
-  //     { type: 'vertical',   x: 630, y: 425, length: 500 }
-  //   ];
-  // }),
-
   absolute: computed('lines', function() {
     let { lines } = this;
-    let pos = (line, prop) => round(line[prop], 0);
-    let length = line => round(line.length, 0);
-    return lines.map(line => ({
-      direction: line.direction,
-      x:      pos(line, 'x'),
-      y:      pos(line, 'y'),
-      length: length(line)
-    }));
+    return lines;
   }).readOnly(),
 
   content: computed('enabled', 'absolute', function() {
