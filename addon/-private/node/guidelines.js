d53e49b7b2d84142dc16d683667ba819140aa3b0
Arnis Vuskans <ampatspell@gmail.com>
2019-05-31 23:56:43 +0300

    Update guidelines.js

--- addon/-private/node/guidelines.js
+++ addon/-private/node/guidelines.js
@@ -9,4 +9,12 @@ export default EmberObject.extend({
     return this.node.edges.all;
   }).readOnly(),
 
+  /*
+
+    So, guidelines has two stages:
+      * nearby guidelines -- those which are say 5px away and needs to be snapped to
+      * equal -- when snapping happens or just postion/size is identical to another edge
+
+  /*
+
 });
