60f34fbe8f5016ab591c6c28cd4811a9082a0aa7
Arnis Vuskans <ampatspell@gmail.com>
2019-07-23 21:05:41 +0300

    move logic to resize state

--- addon/-private/stage/tools/node/resize.js
+++ addon/-private/stage/tools/node/resize.js
@@ -1,115 +1,20 @@
 import Tool from '../-base';
-import { assign } from '@ember/polyfills';
-import { computed } from '@ember/object';
 
 export default Tool.extend({
 
   guidelines: true,
 
-  node: null,
-  edge: null,
-
-  free: computed('keyboard.isShift', 'state.node.attributes.aspect.locked', function() {
-    let locked = !!this.get('state.node.attributes.aspect.locked');
-    let shift = this.get('keyboard.isShift');
-    return locked === shift;
-  }).readOnly(),
-
-  aspect: computed('free', 'state.node.aspect', function() {
-    if(this.free) {
-      return;
-    }
-    return this.state.node.aspect;
-  }).readOnly(),
-
-  updateAspect() {
-    this.state.node.perform('aspect-update');
-  },
-
-  // update(delta) {
-  //   let { node, edge, aspect } = this;
-
-  //   let before = node.frame.properties;
-  //   let frame = assign({}, node.frame.properties);
-  //   let children = {};
-  //   let attributes = node.attributes;
-
-  //   if(edge.vertical === 'bottom') {
-  //     let value = attributes.clampDelta('height', delta.y);
-  //     frame.height += value;
-  //   } else if(edge.vertical === 'top') {
-  //     let value = attributes.clampDelta('height', -delta.y);
-  //     frame.y += -value;
-  //     frame.height += value;
-  //     children.y = value;
-  //   }
-
-  //   if(edge.horizontal === 'right') {
-  //     let value = attributes.clampDelta('width', delta.x);
-  //     frame.width += value;
-  //   } else if(edge.horizontal === 'left') {
-  //     let value = attributes.clampDelta('width', -delta.x);
-  //     frame.x += -value;
-  //     frame.width += value;
-  //     children.x = value;
-  //   }
-
-  //   if(aspect) {
-  //     let height;
-  //     let width;
-
-  //     if(before.width !== frame.width) {
-  //       height = attributes.clamp('height', frame.width / aspect);
-  //       width = attributes.clamp('width', height * aspect);
-  //     } else if(before.height !== frame.height) {
-  //       width = attributes.clamp('width', frame.height * aspect);
-  //       height = attributes.clamp('height', width / aspect);
-  //     }
-
-  //     if(width && height) {
-  //       frame.height = height;
-  //       frame.width = width;
-  //     }
-  //   }
-
-  //   node.update(frame);
-  //   node.nodes.all.forEach(node => node.update(children, { delta: true }));
-
-  //   let after = node.frame.properties;
-  //   frame = {};
-
-  //   if(edge.horizontal === 'left') {
-  //     frame.x = (before.x + before.width) - (after.x + after.width);
-  //   } else if(edge.horizontal === 'middle') {
-  //     frame.x = ((before.x + before.width) - (after.x + after.width)) / 2;
-  //   }
-
-  //   if(edge.vertical === 'top') {
-  //     frame.y = (before.y + before.height) - (after.y + after.height);
-  //   } else if(edge.vertical === 'middle') {
-  //     frame.y = ((before.y + before.height) - (after.y + after.height)) / 2;
-  //   }
-
-  //   node.update(frame, { delta: true });
-
-  //   if(!aspect) {
-  //     this.updateAspect();
-  //   }
-
-  //   node.perform('move-to-container');
-
-  //   return true;
-  // },
+  state: null,
 
   update({ delta }) {
-    let { zoom, aspect } = this;
+    let { zoom } = this;
 
     let zoomed = {
       x: delta.x / zoom,
       y: delta.y / zoom
     };
 
-    this.state.update({ delta: zoomed, aspect });
+    this.state.update({ delta: zoomed });
   },
 
   onMouseMove({ delta }) {
@@ -128,13 +33,13 @@ export default Tool.extend({
 
   onKeyDown() {
     if(!this.free) {
-      this.updateAspect();
+      this.state.updateAspect();
     }
   },
 
   activate({ node }) {
-    let edge = node.edge.serialized;
     this.selection.removeExcept(node);
+    let edge = node.edge.serialized;
     let state = this.stateModel('state', { node, edge });
     this.setProperties({ state });
   },
--- addon/-private/stage/tools/node/resize/state.js
+++ addon/-private/stage/tools/node/resize/state.js
@@ -1,9 +1,12 @@
 import EmberObject, { computed } from '@ember/object';
 import { readOnly } from '@ember/object/computed';
+import { assign } from '@ember/polyfills';
 
 export default EmberObject.extend({
 
   tool: null,
+  keyboard: readOnly('tool.keyboard'),
+
   node: null,
   edge: null,
 
@@ -15,19 +18,97 @@ export default EmberObject.extend({
     this.set('point', { x, y });
   },
 
-  // addDelta(delta) {
-  //   let { point } = this;
-  //   let add = prop => point[prop] = point[prop] + delta[prop];
-  //   add('x');
-  //   add('y');
-  //   return point;
-  // }
+  free: computed('keyboard.isShift', 'node.attributes.aspect.locked', function() {
+    let locked = !!this.get('node.attributes.aspect.locked');
+    let shift = this.get('keyboard.isShift');
+    return locked === shift;
+  }).readOnly(),
 
-  update({ delta, aspect }) {
-    console.log(this.node+'', this.edge, delta, aspect);
-    // let { action, node } = this;
-    // let point = this.addDelta(delta);
-    // action.perform(node, { point });
+  aspect: computed('free', 'node.aspect', function() {
+    let { free, node } = this;
+    if(free) {
+      return;
+    }
+    return node.aspect;
+  }).readOnly(),
+
+  updateAspect() {
+    this.node.perform('aspect-update');
+  },
+
+  update({ delta }) {
+    let { node, edge, aspect } = this;
+
+    let before = node.frame.properties;
+    let frame = assign({}, node.frame.properties);
+    let children = {};
+    let attributes = node.attributes;
+
+    if(edge.vertical === 'bottom') {
+      let value = attributes.clampDelta('height', delta.y);
+      frame.height += value;
+    } else if(edge.vertical === 'top') {
+      let value = attributes.clampDelta('height', -delta.y);
+      frame.y += -value;
+      frame.height += value;
+      children.y = value;
+    }
+
+    if(edge.horizontal === 'right') {
+      let value = attributes.clampDelta('width', delta.x);
+      frame.width += value;
+    } else if(edge.horizontal === 'left') {
+      let value = attributes.clampDelta('width', -delta.x);
+      frame.x += -value;
+      frame.width += value;
+      children.x = value;
+    }
+
+    if(aspect) {
+      let height;
+      let width;
+
+      if(before.width !== frame.width) {
+        height = attributes.clamp('height', frame.width / aspect);
+        width = attributes.clamp('width', height * aspect);
+      } else if(before.height !== frame.height) {
+        width = attributes.clamp('width', frame.height * aspect);
+        height = attributes.clamp('height', width / aspect);
+      }
+
+      if(width && height) {
+        frame.height = height;
+        frame.width = width;
+      }
+    }
+
+    node.update(frame);
+    node.nodes.all.forEach(node => node.update(children, { delta: true }));
+
+    let after = node.frame.properties;
+    frame = {};
+
+    if(edge.horizontal === 'left') {
+      frame.x = (before.x + before.width) - (after.x + after.width);
+    } else if(edge.horizontal === 'middle') {
+      frame.x = ((before.x + before.width) - (after.x + after.width)) / 2;
+    }
+
+    if(edge.vertical === 'top') {
+      frame.y = (before.y + before.height) - (after.y + after.height);
+    } else if(edge.vertical === 'middle') {
+      frame.y = ((before.y + before.height) - (after.y + after.height)) / 2;
+    }
+
+    node.update(frame, { delta: true });
+
+    if(!aspect) {
+      this.updateAspect();
+    }
+
+    node.perform('move-to-container');
+
+    return true;
   }
 
 });
