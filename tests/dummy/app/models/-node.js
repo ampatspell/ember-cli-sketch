e06a48c103b0b787495ce3c640f8ef3c58652480
Arnis Vuskans <ampatspell@gmail.com>
2019-07-25 00:37:23 +0300

    move position, size attrs to sketch

--- tests/dummy/app/models/-node.js
+++ tests/dummy/app/models/-node.js
@@ -1,11 +1,14 @@
-import { node as _node, attr as _attr, prop, guidelines } from 'ember-cli-sketch/computed';
+import { node as _node, prop, guidelines, attrs as _attrs } from 'ember-cli-sketch/computed';
 import { assign } from '@ember/polyfills';
 
 export const node = opts => _node(assign({ identifier: 'dummy' }, opts));
 
-export const attr = (target, opts) => _attr(`doc.${target}`, opts);
+const { attr, position, size } = _attrs(target => `doc.${target}`);
 
 export {
+  attr,
+  position,
+  size,
   prop,
   guidelines
 }
