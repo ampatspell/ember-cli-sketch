8b0b343f75398d1c271552ef420f95c46ec500b8
Arnis Vuskans <ampatspell@gmail.com>
2019-07-25 03:49:09 +0300

    add initial to position attr

--- addon/-private/computed/attrs.js
+++ addon/-private/computed/attrs.js
@@ -4,7 +4,7 @@ import _attr from './attr';
 export default targetFn => {
   let attr = (target, opts) => _attr(targetFn(target), opts);
 
-  let position = (target, opts) => attr(target, assign({ type: 'number', decimals: 1 }, opts));
+  let position = (target, opts) => attr(target, assign({ type: 'number', initial: 0, decimals: 1 }, opts));
   let size = (target, opts) => attr(target, assign({ type: 'number', min: 0, decimals: 1 }, opts));
 
   return {
